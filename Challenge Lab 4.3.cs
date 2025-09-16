47f63078db225dd59c44ed6fdcc0ad8640467283
Chiblood <chi.blood6@gmail.com>
2025-09-16 16:37:14 -0400

    Add initial project files and .gitignore for Challenge Lab 4.3

---
+++ Challenge Lab 4.3.cs
@@ -0,0 +1,37 @@
+﻿/* Challenge Lab 4.3
+3. Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place. (Problem 344 in leetcode)
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+
+Output: ["h","a","n","n","a","H"]
+*/
+public class ChallengeLab4_3
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            // Swap characters at left and right indices
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            // Move towards the middle
+            left++;
+            right--;
+        }
+    }
+}
